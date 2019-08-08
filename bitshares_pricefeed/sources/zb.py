5e378327ce31ebbff9f14e039d6520f74364686e
Zapata <marco.tessari@gmail.com>
2019-08-08 22:17:42 +0200

    ZB url is configurable
        zb:
            klass: Zb
            url: 'http://api.zb.cn'
            quotes:
                - BTS
            bases:
                - QC

--- bitshares_pricefeed/sources/zb.py
+++ bitshares_pricefeed/sources/zb.py
@@ -3,15 +3,19 @@ from . import FeedSource, _request_headers
 
 
 class Zb(FeedSource):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.base_url = getattr(self, 'url', 'http://api.zb.com')
+
     def _fetch(self):
         feed = {}
-        url = "http://api.zb.cn/data/v1/ticker?market={quote}_{base}"
+        url = "{base_url}/data/v1/ticker?market={quote}_{base}"
         for base in self.bases:
             for quote in self.quotes:
                 if base == quote:
                     continue
                 response = requests.get(
-                    url=url.format(base=base, quote=quote),
+                    url=url.format(base_url=self.base_url, base=base, quote=quote),
                     headers=_request_headers,
                     timeout=self.timeout)
                 result = response.json()
