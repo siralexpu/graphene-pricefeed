b0bbe58b714127a55bb3736d18b76b159344acd0
Zapata <marco.tessari@gmail.com>
2019-08-11 18:16:18 +0200

    Update OKCoin API.

--- bitshares_pricefeed/sources/okcoin.py
+++ bitshares_pricefeed/sources/okcoin.py
@@ -10,14 +10,9 @@ class Okcoin(FeedSource):
             for quote in self.quotes:
                 if quote == base:
                     continue
-                if base == "USD":
-                    url = "https://www.okcoin.com/api/v1/ticker.do?symbol=%s_%s" % (quote.lower(), base.lower())
-                elif base == "CNY":
-                    url = "https://www.okcoin.cn/api/ticker.do?symbol=%s_%s" % (quote.lower(), base.lower())
-                else:
-                    sys.exit("\n%s does not know base type %s" % (type(self).__name__, base))
+                url = "https://www.okcoin.com/api/spot/v3/instruments/{}-{}/ticker".format(quote.upper(), base.upper())
                 response = requests.get(url=url, headers=_request_headers, timeout=self.timeout)
                 result = response.json()
-                self.add_rate(feed, base, quote, float(result["ticker"]["last"]), float(result["ticker"]["vol"]))
+                self.add_rate(feed, base, quote, float(result["last"]), float(result["base_volume_24h"]))
                 feed[self.alias(base)]["response"] = result
         return feed
--- tests/sources/test_okcoin.py
+++ tests/sources/test_okcoin.py
@@ -1,8 +1,8 @@
 from bitshares_pricefeed.sources.okcoin import Okcoin
 
 def test_okcoin_fetch(checkers):
-    source = Okcoin(quotes=['BTC'], bases=['USD', 'CNY']) 
+    source = Okcoin(quotes=['BTC'], bases=['USD', 'EUR']) 
     feed = source.fetch()
-    checkers.check_feed(feed, ['BTC:USD', 'BTC:CNY'])
+    checkers.check_feed(feed, ['BTC:USD', 'BTC:EUR'])
 
 
